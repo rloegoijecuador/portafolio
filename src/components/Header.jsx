4d16e5cf7a3f6d140d329419a4e9289dbb2df1be
rloegoijecuador <puyol.black96@gmail.com>
2023-12-06 01:40:51 -0500

    agree more

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import '../styles/Header.css'
+
+const Header = () => {
+  return (
+    <div className='container-fluid'>
+    <ul className="nav justify-content-end">
+  <li className="nav-item">
+    <a clasName="nav-link active" aria-current="page" href="#">home</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#">proyects</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#">ability</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href='#'>contact</a>
+  </li>
+</ul>
+    </div>
+  )
+}
+
+export default Header
